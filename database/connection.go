f85d574113dc9f7e780446b5b203a6c3a9f81b46
Denis Alustau <denisalustau@gmail.com>
2018-06-10 23:34:35 -0300

    Adding .env file

--- database/connection.go
+++ database/connection.go
@@ -2,23 +2,33 @@ package database
 
 import (
 	"log"
+	"os"
 
 	"github.com/jmoiron/sqlx"
 	//not used by application
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var db *sqlx.DB
+var (
+	db *sqlx.DB
+)
 
 //GetConnection return db connection
 func GetConnection() (db *sqlx.DB, err error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbname := os.Getenv("DB_DATABASE")
+	username := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+
 	err = nil
 
 	if db != nil {
 		return
 	}
 
-	db, err = sqlx.Open("mysql", "root:alustau321@tcp(localhost:3306)/issuer?parseTime=true")
+	dbstring := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname + "?parseTime=true"
+	db, err = sqlx.Open("mysql", dbstring)
 
 	if err != nil {
 		log.Println("[GetConnection] Error connection: ", err.Error())
--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/alustau/issuer/router"
+	"github.com/joho/godotenv"
 )
 
 func init() {
 	fmt.Print("Starting server")
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
+
 func main() {
 	fmt.Print("[OK]")
 	router.Routes()
